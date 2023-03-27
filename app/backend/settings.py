0dadf6095a78ccd79d35fb0272a09c27fdd021b6
Zilong <106037081+Zilong007z@users.noreply.github.com>
2023-03-27 12:09:50 +0300

    bref

--- app/backend/settings.py
+++ app/backend/settings.py
@@ -178,7 +178,9 @@ CORS_ALLOWED_ORIGINS = []
 CORS_ALLOWED_ORIGINS.extend(filter(None,os.environ.get('ALLOWED_HOSTS_HTTP', '').split(','),))# If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
 CORS_ALLOWED_ORIGIN_REGEXES = []
 CORS_ALLOWED_ORIGIN_REGEXES.extend(filter(None,os.environ.get('ALLOWED_HOSTS_HTTP', '').split(','),))
+CORS_ORIGIN_WHITELIST = ['http://smartshopcenter.org:3000']
 CORS_ORIGIN_ALLOW_ALL = False#True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
