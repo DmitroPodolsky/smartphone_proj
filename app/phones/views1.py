2e206f22cbdb58caa6e52769bc1cd873e65bd93d
Zilong <106037081+Zilong007z@users.noreply.github.com>
2023-04-03 20:27:29 +0300

    okey

--- app/phones/views1.py
+++ app/phones/views1.py
@@ -5,7 +5,7 @@ from django.template.loader import render_to_string
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMessage, send_mail
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .passwords import generate_password
@@ -49,12 +49,20 @@ def activateEmail(request, user, to_email):
         'token': account_activation_token.make_token(user),
         "protocol": 'https' if request.is_secure() else 'http'
     })
-    email = EmailMessage(mail_subject, message, to=[to_email])
-    if email.send():
+    #email = EmailMessage(mail_subject, message, to=[to_email])
+    send_mail(
+                        subject=mail_subject,
+                        message=f"к сожалению данного продукта пока нету в наличии. вся оплата будет возращена за данный товар. С вами свяжеться техническая поддержка в ближайшое время доставки в ближайшое время",
+                        recipient_list=[to_email],
+                        from_email="matt@test.com",
+                        fail_silently=True,
+                        html_message=message
+                    )
+    '''if email.send():
         messages.success(request, f'Dear <b>{user}</b>, please go to you email <b>{to_email}</b> inbox and click on \
                 received activation link to confirm and complete the registration. <b>Note:</b> Check your spam folder.')
     else:
-        messages.error(request, f'Problem sending email to {to_email}, check if you typed it correctly.')
+        messages.error(request, f'Problem sending email to {to_email}, check if you typed it correctly.')'''
 
 
 def RedirectEmail(request, user, to_email):
